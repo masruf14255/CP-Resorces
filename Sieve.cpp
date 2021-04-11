18db1450e7ef6ca979c425afbb2fe47a82f7da47
Al Mashruf Tonoy <74848229+masruf14255@users.noreply.github.com>
2021-04-12 00:53:14 +0600

    Create Sieve.cpp

---
+++ Sieve.cpp
@@ -0,0 +1,96 @@
+//#Name: Al Mashruf Tonoy
+#include<bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdarg>
+#include <utility>
+#include <bitset>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> vint;
+typedef pair<int,int> pint;
+typedef unsigned long long ULL;
+
+short CC_;
+#define sf scanf
+#define pf printf
+#define NL pf("\n");
+#define PP getchar();
+#define SS printf(">_<LOOOOOK@MEEEEEEEEEEEEEEE<<( %d )>>\n",++CC_);
+#define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
+#define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
+
+//constants
+const int SZ= 1E6;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+
+//function templates
+template <class T>void DA(T* x, int l){for(int i=0; i<l;i++)cout<<"["<<i<<"]>> "<<x[i]<<'\n';}
+template <class T>inline void sary(T* st, T* nd){while(st<nd)cin>>*st++;/*sf("%d", st++);*/}
+template <class T>void tobin(T n,char*bin){int pos= 1<<((int)log2(n));while(pos>=1){if((n & pos)==0)*bin='0';else*bin='1';pos>>=1;bin++;}*bin='\0';}
+template <class T>int strOccur(string&s,T&tgt){int oc= 0, p= s.find(tgt);while(p != string::npos){p = s.find(tgt, p+1);oc++;}return oc;}
+template <class T>LL factorial(T n){LL f= n;while(--n){f*= n;}return f;}
+template <class T>inline T LCM(T x,T y){return((x*y)/__gcd(x,y));}
+int bigMod(int b,int e,int m){if(e==0)return 1;if(!e&1){int temp=bigMod(b,e/2,m)%m;return(temp*temp)%m;}else return((b%m)*(bigMod(b,e-1,m))%m)%m;}
+inline LL POW(LL base, int exp){LL p= 1; while(exp-->0){p *= base;} return p;}
+bool ispal(const string& str){int len= str.length();for(int i= 0; i<len/2; i++){if(str[i]==str[len-i-1]){}else return false;}return true;}
+bool comp(const int& a,const int& b){return a>b;}
+
+
+bool p[1000000];
+
+bool isPrime(int n)
+{
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    return p[n] == false;
+}
+void sieve(int n)//3
+{
+    for (int i = 3; i * i <= n; i += 2)//5
+    {
+        if (p[i] == false)
+        {
+            for (int j = i * i; j <= n; j += i + i)
+            {
+                p[j] = true;
+            }
+        }
+    }
+}
+int main()
+{
+    int k,num;
+    cin>>k;//2
+    sieve(1000000);
+    vector<int> result;
+    for(int i = 0; i<1000000; i++)
+        if(isPrime(i))//3
+            result.push_back(i);//3
+    while(k-->0)//2-1->1
+    {
+        cin>>num;
+        cout<<result[num-1]<<endl;//5
+    }
+}
+
